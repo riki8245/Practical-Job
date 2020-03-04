d3ff71cf634afcd2819be3d3b9775a57b554a614
allstendres98 <allstendres98@users.noreply.github.com>
2020-03-04 13:39:47 +0100

    presure plate

---
+++ Assets/Alex/Scripts/pressurePlateController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressurePlateController : MonoBehaviour
+{
+    private bool pressed = false;
+    private Vector3 scaleNotPressed = new Vector3(0.67f, 0.07f, 0.67f);
+    private Vector3 scalePressed = new Vector3(0.67f, 0.01f, 0.67f);
+
+    public Animator door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pressed)
+        {
+            transform.localScale = scalePressed;
+        }
+        else
+        {
+            transform.localScale = scaleNotPressed;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            pressed = true;
+            door.SetBool("open",true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            pressed = false;
+            door.SetBool("open", false);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            pressed = true;
+            door.SetBool("open", true);
+        }
+    }
+
+}
