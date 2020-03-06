c919caf2490fe886cb96b36fdab0b3242bf9c56b
allstendres98 <allstendres@users.noreply.github.com>
2020-03-06 18:18:13 +0100

    movimiento enemigos
    con el circulo se cambia la cara y lo que tiene que hacer el enemigo, faltaría ver que a veces se peta cuando sube una rampa (eso es cosa del bake, creo) pero en general va bien

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform Player;
+
+    private NavMeshAgent nav;
+    private Transform startTransform;
+    private float multiplyBy = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        int PlayerFaceState = PlayerFace.FaceState;
+        if(PlayerFaceState == 0)
+        {
+            nav.enabled = false;
+        }
+        else if (PlayerFaceState == 1)
+        {
+            nav.enabled = true;
+            if (Vector3.Distance(transform.position, Player.position) > 1.5) nav.SetDestination(Player.position);
+            else nav.enabled = false;
+        }
+        else
+        {
+            nav.enabled = true;
+            RunFrom();
+        }
+    }
+
+
+    void RunFrom()
+    {
+
+        // store the starting transform
+        startTransform = transform;
+
+        //temporarily point the object to look away from the player
+        transform.rotation = Quaternion.LookRotation(transform.position - Player.position);
+
+        //Then we'll get the position on that rotation that's multiplyBy down the path (you could set a Random.range
+        // for this if you want variable results) and store it in a new Vector3 called runTo
+        Vector3 runTo = transform.position + transform.forward * multiplyBy;
+        //Debug.Log("runTo = " + runTo);
+
+        //So now we've got a Vector3 to run to and we can transfer that to a location on the NavMesh with samplePosition.
+
+        NavMeshHit hit;    // stores the output in a variable called hit
+
+        // 5 is the distance to check, assumes you use default for the NavMesh Layer name
+        NavMesh.SamplePosition(runTo, out hit, 5, 1 << NavMesh.GetAreaFromName("Walkable"));
+        //Debug.Log("hit = " + hit + " hit.position = " + hit.position);
+
+        // just used for testing - safe to ignore
+       // nextTurnTime = Time.time + 5;
+
+        // reset the transform back to our start transform
+        transform.position = startTransform.position;
+        transform.rotation = startTransform.rotation;
+
+        // And get it to head towards the found NavMesh position
+        nav.SetDestination(hit.position);
+    }
+}
+
---
+++ Assets/PlayerFace.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFace : MonoBehaviour
+{
+    public static int FaceState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FaceState = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire2")) ChangeFaceState();
+        //print(FaceState);
+    }
+
+    void ChangeFaceState()
+    {
+        if (FaceState == 2) FaceState = 0;
+        else FaceState++;
+    }
+}
