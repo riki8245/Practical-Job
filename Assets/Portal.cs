2fe270fcf5925d427e195c94cb07da1e93a1f17e
riki8245 <56740262+riki8245@users.noreply.github.com>
2020-04-03 00:29:30 +0200

    character

---
+++ Assets/Portal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private Transform destination;
+    private Transform pass;
+
+    public bool isPortal_1;
+    void Start()
+    {
+        if (!isPortal_1)
+        {
+            destination = GameObject.FindGameObjectWithTag("Portal_1").GetComponent<Transform>();
+        }
+        else
+        {
+            destination = GameObject.FindGameObjectWithTag("Portal_2").GetComponent<Transform>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}                                                                                              
