3fa4e6bb7af608d5f4a839f959c49033d1c8b97b
riki8245 <56740262+riki8245@users.noreply.github.com>
2020-04-03 16:24:59 +0200

    animation script

---
+++ Assets/AnimationScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    private PlayerController controller;
+    private CharacterController ch_controller;
+    private Animator ch_anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ch_controller = GetComponent<CharacterController>();
+        controller = GetComponent<PlayerController>();
+        ch_anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ch_anim.SetBool("pushing", controller.p_PushingOrPulling);
+        ch_anim.SetFloat("speed", ch_controller.velocity.magnitude);
+        ch_anim.SetBool("joyinput", ch_controller.velocity.magnitude > 0.1f? Input.GetButton("L3") : false);
+    }
+}
