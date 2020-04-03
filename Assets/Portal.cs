a821d1e068ce6212043e123cc13fd85d923866ba
riki8245 <56740262+riki8245@users.noreply.github.com>
2020-04-03 04:40:29 +0200

    la cosa de duplicar el objeto mientras se teleporta
    la logica que necesita esta mas o menos, en plan conceptual (muy conceptual), pero esto fixeado funciona ^^

--- Assets/Portal.cs
+++ Assets/Portal.cs
@@ -10,12 +10,20 @@ public class Portal : MonoBehaviour
     private bool passingTrough;
     private Vector3 newVelocity;
     private bool teleported;
+    public GameObject myPrefab;
+    private GameObject boxcopy;
+    public float distance;
+    public float dis_to_reset_col;
 
 
 
     public bool isPortal_1;
+
+    //This is A mierda, PLEASE check it
     void Start()
     {
+        dis_to_reset_col = 0.2f;
+        distance = 0.15f;
         passingTrough = false;
         teleported = false;
         if (!isPortal_1)
@@ -34,17 +42,18 @@ public class Portal : MonoBehaviour
         if (passingTrough)
         {
             newVelocity = box.GetComponent<Rigidbody>().velocity;
-            if (transform.position.y > box.transform.position.y)
+            if (transform.position.y + distance > box.transform.position.y)
             {
-                box.transform.position = new Vector3(destination.position.x, destination.position.y, destination.position.z);
+                box.transform.position = new Vector3(destination.position.x-distance, destination.position.y, destination.position.z);
                 box.GetComponent<Rigidbody>().velocity = new Vector3(-newVelocity.y, newVelocity.x, newVelocity.z);
                 //box.GetComponent<BoxCollider>().enabled = true;
                 teleported = true;
             }
             if (teleported)
             {
-                if(box.transform.position.x > destination.position.x + 0.5f)
+                if(box.transform.position.x > destination.position.x + dis_to_reset_col)
                 {
+                    boxcopy.active = false;
                     box.GetComponent<BoxCollider>().enabled = true;
                     passingTrough = false;
                     teleported = false;
@@ -61,6 +70,9 @@ public class Portal : MonoBehaviour
             box = other.gameObject;
             box.GetComponent<BoxCollider>().enabled = false;
             passingTrough = true;
+            boxcopy= Instantiate(myPrefab, box.transform.position, box.transform.rotation);
+            boxcopy.GetComponent<BoxCollider>().enabled = false;
+            boxcopy.GetComponent<Rigidbody>().velocity = box.GetComponent<Rigidbody>().velocity;
         }
     
     }
