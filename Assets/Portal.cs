07ade1db1f1b327734b6d3728e53ca87ce2a5eaf
riki8245 <56740262+riki8245@users.noreply.github.com>
2020-04-03 04:17:57 +0200

    portal work so roughly (need support, please)
    Lo he hecho a fuerza bruta funciona si el portal 1 esta en el suelo, y solo del portal uno al 2, no al reves.
    Basicamente lleva un trigger, que desactiva el collider de la caja, si mi position en el vector por el que este entrando en este caso y, es mayor, me teletransporta, y una vez me he salido del collider 1f me lo vuelve a activar.
    La cosa en realidad seria desactivar el collider del muro en si, pero solo para la box, y detectar por donde cojones esta entrando para no hacerlo tan marronero. Y esto al hacerlo con el controller de unity imagino que variara y sera mas manejable. Asi que yo creo que esto va okei como test.

--- Assets/Portal.cs
+++ Assets/Portal.cs
@@ -6,10 +6,18 @@ public class Portal : MonoBehaviour
 {
     private Transform destination;
     private Transform pass;
+    private GameObject box;
+    private bool passingTrough;
+    private Vector3 newVelocity;
+    private bool teleported;
+
+
 
     public bool isPortal_1;
     void Start()
     {
+        passingTrough = false;
+        teleported = false;
         if (!isPortal_1)
         {
             destination = GameObject.FindGameObjectWithTag("Portal_1").GetComponent<Transform>();
@@ -23,6 +31,37 @@ public class Portal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (passingTrough)
+        {
+            newVelocity = box.GetComponent<Rigidbody>().velocity;
+            if (transform.position.y > box.transform.position.y)
+            {
+                box.transform.position = new Vector3(destination.position.x, destination.position.y, destination.position.z);
+                box.GetComponent<Rigidbody>().velocity = new Vector3(-newVelocity.y, newVelocity.x, newVelocity.z);
+                //box.GetComponent<BoxCollider>().enabled = true;
+                teleported = true;
+            }
+            if (teleported)
+            {
+                if(box.transform.position.x > destination.position.x + 0.5f)
+                {
+                    box.GetComponent<BoxCollider>().enabled = true;
+                    passingTrough = false;
+                    teleported = false;
+                }
+            }
+            
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Box"))
+        {
+            box = other.gameObject;
+            box.GetComponent<BoxCollider>().enabled = false;
+            passingTrough = true;
+        }
+    
     }
 }                                                                                              
