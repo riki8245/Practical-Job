2366ed36e342741e6fbeda986eb98df87a87204b
allstendres98 <allstendres@users.noreply.github.com>
2020-05-08 12:44:04 +0200

    nivel 1 y 2 montado y cambiar de nivel

---
+++ Assets/Alex/Scripts/finishLevel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class finishLevel : MonoBehaviour
+{
+    private bool playerReachFinish = false;
+    private float timeToPassToNextLevel;
+    public Animator door;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerReachFinish)
+        {
+            timeToPassToNextLevel += Time.deltaTime;
+            door.SetBool("open", false);
+            player.GetComponent<Animator>().SetFloat("speed", player.GetComponent<Animator>().speed - timeToPassToNextLevel*1000);
+            if (timeToPassToNextLevel > 2) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerReachFinish = true;
+            player.GetComponent<PlayerController>().enabled = false;
+        }
+    }
+}
