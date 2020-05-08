c21f5991b802e82dc0729e0b0b161beaf477b2e1
victor13alvarez <55243827+victor13alvarez@users.noreply.github.com>
2020-05-08 14:20:54 +0200

    MainScene

---
+++ Assets/Victor/Scenes/MainScene/ButtonsCanvasAnimator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsCanvasAnimator : MonoBehaviour
+{
+    private void Awake()
+    {
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, -10);
+        this.gameObject.transform.localScale = new Vector3(0.1f, 0.0f, 0.1f);
+
+    }
+    public MS_enemyMove mS_EnemyMove;
+    private void OnEnable()
+    {
+        this.gameObject.SetActive(true);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        iTween.MoveTo(this.transform.gameObject, iTween.Hash("z", 4.840001, "easeType", "easeOutBounce", "time", 2f));
+        iTween.ScaleTo(this.transform.gameObject, iTween.Hash("x", 0.1, "y", 0.1, "z", 0.1,"easeType","easeOutSine","time",2f,"oncomplete","moveButtons"));
+    }
+
+    // Update is called once per frame
+    private void moveButtons()
+    {
+        Vector3[] path = {
+        new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z),
+        new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + .8f, this.gameObject.transform.position.z),
+        new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y - .8f, this.gameObject.transform.position.z) ,
+        new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z)};
+
+        iTween.MoveTo(this.transform.gameObject, iTween.Hash("path",path,"time", 5f, "easeType","linear","oncomplete","moveButtons"));
+        mS_EnemyMove.enabled = true;
+    }
+}
---
+++ Assets/Victor/Scenes/MainScene/MS_enemyMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MS_enemyMove : MonoBehaviour
+{
+    private NavMeshAgent e_nav;
+    private Rigidbody e_rb;
+    private Animator e_anim;
+    private int e_GoTo;
+    public Transform pointA;
+    public Transform pointB;
+
+    private Vector3 e_PointA;
+    private Vector3 e_PointB;
+
+    private void Awake()
+    {
+        e_nav = GetComponent<NavMeshAgent>();
+        e_rb = GetComponent<Rigidbody>();
+        e_anim = GetComponent<Animator>();
+        e_GoTo = 1;
+        e_PointA = pointA.position;
+        e_PointB = pointB.position;
+    }
+    private void OnEnable()
+    {
+        e_nav.enabled = true;
+        e_nav.SetDestination(e_PointB);
+    }
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, e_PointA) < 1f && e_GoTo == 0)
+        {
+            e_nav.enabled = false;
+            iTween.RotateTo(this.gameObject, iTween.Hash("y", 0f, "oncomplete", "changeTargetPoint"));
+        }
+        else if (Vector3.Distance(this.transform.position, e_PointB) < 1f && e_GoTo == 1) {
+            e_nav.enabled = false;
+            iTween.RotateTo(this.gameObject, iTween.Hash("y", 180f, "oncomplete", "changeTargetPoint"));
+        }
+        e_anim.SetFloat("Speed", e_nav.velocity.magnitude);
+    }
+    private void changeTargetPoint()
+    {
+        e_nav.enabled = true;
+        e_nav.isStopped = true;
+        if (e_GoTo == 0) {
+            e_GoTo = 1; e_nav.SetDestination(e_PointB);
+        } 
+        else {
+            e_GoTo = 0; e_nav.SetDestination(e_PointA);
+        }
+        e_nav.isStopped = false;
+
+    }
+}
