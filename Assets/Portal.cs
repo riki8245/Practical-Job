88f0398fbccf86f8eaac1352c8f38d7cea83976f
riki8245 <56740262+riki8245@users.noreply.github.com>
2020-05-08 23:04:52 +0200

    Portals
    Portal has a plane without mess renderer to know where his normal is facing.
    WORKS!

--- Assets/Portal.cs
+++ Assets/Portal.cs
@@ -4,31 +4,38 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+
     private Transform destination;
     private Transform pass;
     private GameObject box;
     private bool passingTrough;
     private Vector3 newVelocity;
     private bool teleported;
-    public GameObject myPrefab;
     private GameObject boxcopy;
+    private Vector3 originalBoxtransformPos;
+    private Quaternion originalBoxrotation;
+    private Vector3 auxDir;
+
+    public GameObject myPrefab;
     public float distance;
     public float dis_to_reset_col;
     public int layerBoxes;
     public int layerPassable;
     public int layerFloor;
-    Vector3 originalBoxtransformPos;
-    Quaternion originalBoxrotation;
-    Vector3 auxDir;
 
-    private GameObject directionObject;
+    //Raycast
+    [SerializeField] private GameObject directionObject;
+    [SerializeField] private LayerMask LayersToCast;
+    [HideInInspector] public string portal_face = "";
+    private RaycastHit ray;
+
 
     public bool isPortal_1;
 
     //This is A mierda, PLEASE check it
     void Start()
     {
-        directionObject = GetComponentInChildren<Transform>().gameObject;
+        calculatePortalPosition();
         dis_to_reset_col = 0.2f;
         passingTrough = false;
         teleported = false;
@@ -38,16 +45,41 @@ public class Portal : MonoBehaviour
             destination = GameObject.FindGameObjectWithTag("Portal_2").GetComponent<Transform>();
     }
 
+    private void calculatePortalPosition()
+    {
+        //Esto ya funciona (solo he puesto esas posiciones porque son las unicas en las que se ve el portal en isometrica)
+        Physics.Raycast(directionObject.transform.position, -directionObject.transform.forward, out ray, 10.0f, LayersToCast);
+        Debug.DrawLine(directionObject.transform.position, ray.point);
+        if (ray.normal.y == 1f)
+        {
+            portal_face = "y+";
+        }
+        else if (ray.normal.z == 1f)
+        {
+            portal_face = "z+";
+        }
+        else if (ray.normal.x == 1f)
+        {
+            portal_face = "x+";
+        }
+        /*
+        else if (ray.normal.Equals(Vector3.right))
+        {
+            portal_face = "right(x-)";
+        }
+        */
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-       
+        print("Portal is facing: " + portal_face);
         if (passingTrough)
         {  
             if (this.transform.position.y + .2f > box.transform.position.y  && !teleported)
             {
                 boxcopy = Instantiate(myPrefab, new Vector3(destination.position.x - .6f, destination.position.y-.8f, destination.position.z), originalBoxrotation);
-
                 boxcopy.layer = layerPassable;
                 boxcopy.GetComponent<BoxController>().b_moveDirection = new Vector3(-auxDir.y + 2f, 0f, 0f);
                 teleported = true;
@@ -84,13 +116,14 @@ public class Portal : MonoBehaviour
         }
 
     }
-    //private void OnGUI()
-    //{
-    //    GUIStyle guiStyle = new GUIStyle(); //create a new variable
-    //    guiStyle.fontSize = 30;
-    //    if(box != null)GUI.Label(new Rect(10, 100, 500, 100), "Box: " + box.transform.position.y , guiStyle);
-    //    GUI.Label(new Rect(10, 200, 500, 100), "Portal: " + this.transform.position.y, guiStyle);
+   /* private void OnGUI()
+    {
+        GUIStyle guiStyle = new GUIStyle(); //create a new variable
+        guiStyle.fontSize = 30;
+        //if(box != null)GUI.Label(new Rect(10, 100, 500, 100), "Box: " + box.transform.position.y , guiStyle);
+        // GUI.Label(new Rect(10, 200, 500, 100), "Portal: " + this.transform.position.y, guiStyle);
+        GUI.Label(new Rect(10, 200, 500, 100), "Portal: " + ray.normal, guiStyle);
 
-
-    //}
+    }
+    */
 }                                                                                              
