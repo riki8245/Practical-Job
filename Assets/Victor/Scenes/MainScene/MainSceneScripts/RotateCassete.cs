973f2c9e2cacc34e7c218c08a06b5c290be9c3f9
victor13alvarez <55243827+victor13alvarez@users.noreply.github.com>
2020-05-11 01:48:11 +0200

    Mas cosas del menu principal
    Queda LOD
    Transicion de camara
    About
    Move Player

---
+++ Assets/Victor/Scenes/MainScene/MainSceneScripts/RotateCassete.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCassete : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(20f * Time.deltaTime, 20f * Time.deltaTime, 20f * Time.deltaTime);
+    }
+}
