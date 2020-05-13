1c5fd7091faddef3e0e6c6a5dc89d7abb7a33578
riki8245 <56740262+riki8245@users.noreply.github.com>
2020-05-14 00:07:09 +0200

    Save y outline

---
+++ Assets/Ricky/Scripts/GameData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData : MonoBehaviour
+{
+    public bool c_enableHDR;
+    public bool c_enableMSAA;
+    public bool c_enableShadows;
+    public int currentLevel;
+
+    public GameData(GameManager gameManager) 
+    {
+        this.c_enableHDR = gameManager.c_enableHDR;
+        this.c_enableMSAA = gameManager.c_enableMSAA;
+        this.c_enableShadows = gameManager.c_enableShadows;
+        this.currentLevel = gameManager.currentLevel;
+    }
+}
---
+++ Assets/Ricky/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("PlayerSettings")]
+    public bool c_enableHDR;
+    public bool c_enableMSAA;
+    public bool c_enableShadows;
+    public int  currentLevel;
+
+
+    public void SaveGame()
+    {
+        SaveSystem.SaveGame(this);
+    }
+
+    public void LoadGame()
+    {
+        GameData data = SaveSystem.LoadGame();
+
+        this.c_enableHDR     = data.c_enableHDR;
+        this.c_enableMSAA    = data.c_enableMSAA;
+        this.c_enableShadows = data.c_enableShadows;
+        this.currentLevel    = data.currentLevel;
+    }
+
+}
