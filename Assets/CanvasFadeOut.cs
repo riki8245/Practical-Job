f029bb958718a6109c5807c61ccf6c64b083a4b1
victor13alvarez <55243827+victor13alvarez@users.noreply.github.com>
2020-06-04 17:48:07 +0200

    ricky callate

---
+++ Assets/CanvasFadeOut.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasFadeOut : MonoBehaviour
+{
+    Color color = new Color();
+    float duration;
+    float timer;
+    private void Awake()
+    {
+        color = Color.black;
+        duration = 3f;
+        timer = 0f;
+    }
+    void Update()
+    {
+        color = Color.Lerp(Color.black, new Color(0f, 0f, 0f, 0f), timer);
+        this.GetComponentInChildren<Image>().color = color;
+        if (color.Equals(new Color(0, 0, 0, 0))) Destroy(this);
+        else timer += Time.deltaTime / duration;
+    }
+}
